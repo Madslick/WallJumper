ce61dea1fe591c1825e1e51d535b0c36efc5bf5d
Joe Auby <joeyauby@gmail.com>
2025-07-14 11:31:32 -0700

    Compiling on Android again as well as Desktop!

---
+++ android/src/main/java/io/bitbucket/rift_runner/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package io.bitbucket.rift_runner.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.bitbucket.rift_runner.WallJumper;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new WallJumper(), configuration);
+    }
+}
